c7e9a3e48d4cde6d2a44ffaa0dbd6dd96f8b7f52
shhuuaib <u19cn23s0032.bca@gmail.com>
2024-11-19 11:01:24 +0530

    xdgds

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+const cors = require("cors");
+app.use * (cors());
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+app.listen(port, () => {
+    console.log('Exsmple app listening on port $ {port}')
+})
+
